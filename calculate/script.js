f74fdfecf0dbe3a428bace8983d2c2d2d053d01a
Best <karami52468@gmail.com>
2024-12-11 20:10:02 +0700

    updata

--- calculate/script.js
+++ calculate/script.js
@@ -22,8 +22,16 @@ document.getElementById("tap").onclick = function () {
   int1 = parseInt(result, 10);
   if (x == "เงินที่เหลือ") {
     document.getElementById("my-cash").innerText = result;
+    document.getElementById("my-cash").style.color = "#dd75b5";
   } else if (x == "วันนี้ใช้เงิน") {
-    p += int1;
-    document.getElementById("use-cash").innerText = p;
+    if (!isNaN(int1)) {
+      p += int1;
+      document.getElementById("use-cash").innerHTML = p;
+      document.getElementById("use-cash").style.color = "cyan";
+    } else {
+      document.getElementById("use-cash").innerHTML =
+        "กรุณากรอกตัวเลขที่ถูกต้อง!";
+      document.getElementById("use-cash").style.color = "red";
+    }
   }
 };
